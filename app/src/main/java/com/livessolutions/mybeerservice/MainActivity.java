3567e76eae0152e363ad9838dba5b387b37156f7
NaiBeerAD <atitheppremphu@gmail.com>
2017-11-18 16:06:02 +0700

    Finish 18Nov

--- app/src/main/java/com/livessolutions/mybeerservice/MainActivity.java
+++ app/src/main/java/com/livessolutions/mybeerservice/MainActivity.java
@@ -3,11 +3,22 @@ package com.livessolutions.mybeerservice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.livessolutions.mybeerservice.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment To Activity
+          if (savedInstanceState == null){
+//              Status True
+              getSupportFragmentManager().beginTransaction()
+               .add(R.id.contentFragmentMain, new MainFragment()).commit();
+          }
+
+    }   // Main Method
+
+}   // Main Class
