90668f2e7dcd04612597459ab134281605cabded
NaiBeerAD <atitheppremphu@gmail.com>
2017-11-19 16:06:00 +0700

    Drawer Menu

--- app/src/main/java/com/livessolutions/mybeerservice/MainActivity.java
+++ app/src/main/java/com/livessolutions/mybeerservice/MainActivity.java
@@ -1,24 +1,64 @@
 package com.livessolutions.mybeerservice;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.livessolutions.mybeerservice.fragment.MainFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+                                //Ctl ??? Click
+
 
 //        Add Fragment To Activity
-          if (savedInstanceState == null){
+        if (savedInstanceState == null) {
 //              Status True
-              getSupportFragmentManager().beginTransaction()
-               .add(R.id.contentFragmentMain, new MainFragment()).commit();
-          }
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentFragmentMain, new MainFragment()).commit();
+        } //if
+
+//          Drawer Controller
+        drawerController();
+
+
 
     }   // Main Method
+    //Crl Alt M
+    private void drawerController() {
+        TextView mainTextView = findViewById(R.id.txtMain);
+        TextView secondTextView = findViewById(R.id.txtSecond);
+        TextView exiTextView = findViewById(R.id.txtExit);
+
+        mainTextView.setOnClickListener(this);
+        secondTextView.setOnClickListener(this);
+        exiTextView.setOnClickListener(this);
+
 
 }   // Main Class
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.txtMain:
+
+                break;
+            case R.id.txtSecond:
+
+                break;
+
+            case R.id.txtExit:
+
+                break;
+
+        }
+
+    }
+
+    }
